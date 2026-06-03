54f573e7b136a39ada78bb1013ace4a3e2049763
hugoc <hugo.castbar@gmail.com>
2026-06-03 15:57:14 +0200

    settings frontend, falta parte del backend

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from config import Config
 from auth import auth_bp
+from db_models import db, Setting
 from monitor import monitor_bp, streams_bp, iniciar_logs, iniciar_resources, iniciar_process, iniciar_disks
 
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
     
-    # Configuración
+    # Configuración de SQLite
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    # Configuración de flask
     app.config.from_object(Config)
     app.secret_key = Config.SECRET_KEY
     Config.validate()
